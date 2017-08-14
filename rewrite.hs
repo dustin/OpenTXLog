386b4948beea30f7675e98cf45e49ff8bd8acdf2
dustin <dustin@spy.net>
2017-08-14 06:01:26 +0000

    Use $ more consistently

--- rewrite.hs
+++ rewrite.hs
@@ -10,7 +10,7 @@ processRow :: (V.Vector String) -> [V.Vector String] -> IO ()
 processRow hdr [] = return ()
 processRow hdr (x:xs) =
   do
-    let coords = toCoord $ map (\x -> read x :: Float) $ words (x V.! 11)
+    let coords = toCoord $ map (\x -> read x :: Float) $ words $ x V.! 11
     putStrLn $ show x
     putStrLn $ show coords
     processRow hdr xs
@@ -24,4 +24,4 @@ main = do
     csvData <- BL.readFile "test.csv"
     case decode NoHeader csvData :: Either String (V.Vector (V.Vector String)) of
         Left err -> putStrLn err
-        Right v -> process (V.head v) $ take 25 $ V.toList (V.tail v)
+        Right v -> process (V.head v) $ take 25 $ V.toList $ V.tail v
