32aff8e90c3c475bde56216c9d074236ade15965
dustin <dustin@spy.net>
2017-08-14 05:32:46 +0000

    Parse the coordinates from each row

--- rewrite.hs
+++ rewrite.hs
@@ -8,12 +8,17 @@ import qualified Data.Vector as V
 import System.Environment
 import GHC.Generics (Generic)
 
-processRow :: (V.Vector ByteString) -> [V.Vector ByteString] -> IO ()
+data Coord = Coord { lat :: !Float , lon :: !Float } deriving (Show)
+
+toCoord [lat, lon] = Coord lat lon
+
+processRow :: (V.Vector String) -> [V.Vector String] -> IO ()
 processRow hdr [] = return ()
 processRow hdr (x:xs) =
   do
+    let coords = toCoord $ map (\x -> read x :: Float) $ words (x V.! 11)
     putStrLn $ show x
-    putStrLn $ show $ x V.! 11
+    putStrLn $ show coords
     processRow hdr xs
 
 process hdr vals = do
@@ -23,6 +28,6 @@ process hdr vals = do
 main :: IO ()
 main = do
     csvData <- BL.readFile "test.csv"
-    case decode NoHeader csvData :: Either String (V.Vector (V.Vector ByteString)) of
+    case decode NoHeader csvData :: Either String (V.Vector (V.Vector String)) of
         Left err -> putStrLn err
         Right v -> process (V.head v) $ take 25 $ V.toList (V.tail v)
