fdad491068e331debfb3748638a37bae054b6562
dustin <dustin@spy.net>
2017-08-16 03:03:40 +0000

    Removed some unnecessary parens

--- rewrite.hs
+++ rewrite.hs
@@ -39,7 +39,7 @@ speed ts1 ts2 pos1 pos2 =
   let tΔ = (realToFrac $ diffUTCTime ts1 ts2) D.*~ second
       pΔ = distance pos1 pos2
       ε = 1 D.*~ second in
-    (if (tΔ <= ε) then D._0 else (pΔ D./ tΔ)) D./~ (kilo meter D./ hour)
+    (if tΔ <= ε then D._0 else pΔ D./ tΔ) D./~ (kilo meter D./ hour)
 
 -- Create a FieldLookup function to look up fields in a row by name (based on the header row)
 byName :: (V.Vector String) -> FieldLookup
