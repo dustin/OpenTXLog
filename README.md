6dbbd210344aa1c3bb78faf9dfc67c27eed6755e
dustin <dustin@spy.net>
2017-08-19 00:47:58 +0000

    Converted to a stack project.

---
+++ README.md
@@ -0,0 +1,1 @@
+# OpenTXLog
---
+++ test/Spec.hs
@@ -0,0 +1,12 @@
+import OpenTXLog
+
+import Test.HUnit
+import Test.QuickCheck
+import Test.QuickCheck.Arbitrary
+import Test.Framework (defaultMain, testGroup)
+import Test.Framework.Providers.HUnit
+import Test.Framework.Providers.QuickCheck2 (testProperty)
+
+tests = []
+
+main = defaultMain tests
