f5916741b8683be8e8c2ea1a302a648325023207
Dustin Sallings <dustin@spy.net>
2018-07-09 22:21:45 -0700

    Use optparse-applicative to pass along the ground altitude.

--- app/Main.hs
+++ app/Main.hs
@@ -6,11 +6,31 @@
 import OpenTXLog
 
 import Data.Csv (encode)
-import System.Environment (getArgs)
 import qualified Data.ByteString.Lazy as BL
+
+import Options.Applicative (option, auto, long, showDefault, value, help, helper, fullDesc,
+                            progDesc, argument, metavar, execParser, info, str,
+                            (<**>), Parser)
+import Data.Semigroup ((<>))
+
+data Options = Options { _optGroundAlt :: Int
+                       , _filename :: String
+  }
+
+options :: Parser Options
+options = Options
+  <$> option auto (long "groundAlt" <> showDefault <> value 0 <> help "altitude at takeoff")
+  <*> argument str (metavar "FILE")
+
+doFile :: Options -> IO ()
+doFile (Options alt fn) = do
+  stuff <- processCSVFile fn [(intFieldTransformer "Alt(m)" (+ alt))]
+  (BL.putStr . encode) stuff
 
 main :: IO ()
 main = do
-  file <- head <$> getArgs
-  stuff <- processCSVFile file []
-  (BL.putStr . encode) stuff
+  o <- execParser opts
+  doFile o
+
+  where opts = info (options <**> helper) (fullDesc <> progDesc "fix up telemetry.")
+
